a5a1a70f0a76266a163bebb566679eda725fda38
HeitorPD25 <heitorpdte@gmail.com>
2025-03-14 11:40:22 -0300

    Criado Post e GetAll

--- src/main/java/br/com/fiap/project_digital_bank/controller/CountController.java
+++ src/main/java/br/com/fiap/project_digital_bank/controller/CountController.java
@@ -1,5 +1,54 @@
 package br.com.fiap.project_digital_bank.controller;
 
+import java.util.List;
+//import java.util.logging.Logger;
+
+//import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import br.com.fiap.project_digital_bank.model.Count;
+import br.com.fiap.project_digital_bank.repository.CountRepository;
+
+@RestController
+@RequestMapping("counts")
 public class CountController {
+  
+    //private final Logger log = (Logger) LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    private CountRepository repository;
+
+
+    @GetMapping
+    public List<Count> index() {
+        return repository.findAll();
+    }
+
+
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Count create(@RequestBody Count count) {
+        //log.info("Cadastrando uma nova conta: " + count.getAgency());
+        System.out.println("Cadastrando uma nova conta: " + count.getAgency());
+        repository.save(count);
+        return count;
+    }
+
+
     
+        
+    
+
+
+
+
+
+
+
 }
--- src/main/java/br/com/fiap/project_digital_bank/model/Count.java
+++ src/main/java/br/com/fiap/project_digital_bank/model/Count.java
@@ -2,9 +2,13 @@ package br.com.fiap.project_digital_bank.model;
 
 import java.time.LocalDate;
 
-import org.springframework.data.annotation.Id;
+
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.Data;
 
 @Entity
@@ -13,11 +17,21 @@ public class Count {
 
     @Id
     private String agency;
+
+    @NotNull(message = "O Nome não pode ser nulo.")
     private String name;
-    private int cpf;
+
+    @NotNull(message = "O CPF não pode ser nulo.")
+    private Long cpf;
+    
+    @PastOrPresent(message = "A Data não pode ser no futuro.")
     private LocalDate startDate;
-    private int initialBalance;
+    
+    @PositiveOrZero(message = "O Saldo inicial não pode ser negativo.")
+    private Long initialBalance;
+    
     private boolean status;
-    private Type tipo;
+    
+    private Type type;
     
 }
