870ebe950b0d951ebeade2a9f5ea2ebbd3758e4a
HeitorPD25 <heitorpdte@gmail.com>
2025-03-14 10:11:45 -0300

    Primeiro Commit

---
+++ project-digital-bank/src/main/java/br/com/fiap/project_digital_bank/ProjectDigitalBankApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.project_digital_bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectDigitalBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectDigitalBankApplication.class, args);
+	}
+
+}
---
+++ project-digital-bank/src/test/java/br/com/fiap/project_digital_bank/ProjectDigitalBankApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.project_digital_bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectDigitalBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
