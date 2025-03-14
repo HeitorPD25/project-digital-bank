10ed15b261fca463da99e25b8788f7555ee6b29f
HeitorPD25 <heitorpdte@gmail.com>
2025-03-14 12:02:54 -0300

    GetByAgency e GetByCpf

--- src/main/java/br/com/fiap/project_digital_bank/controller/CountController.java
+++ src/main/java/br/com/fiap/project_digital_bank/controller/CountController.java
@@ -6,12 +6,16 @@ import java.util.List;
 //import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
 import br.com.fiap.project_digital_bank.model.Count;
 import br.com.fiap.project_digital_bank.repository.CountRepository;
 
@@ -30,7 +34,6 @@ public class CountController {
         return repository.findAll();
     }
 
-
     @PostMapping
     @ResponseStatus(code = HttpStatus.CREATED)
     public Count create(@RequestBody Count count) {
@@ -38,6 +41,33 @@ public class CountController {
         System.out.println("Cadastrando uma nova conta: " + count.getAgency());
         repository.save(count);
         return count;
+    }
+
+    @GetMapping("/{agency}")
+    public ResponseEntity<Count> get(@PathVariable String agency) {
+        //log.info("Buscando conta: " + agency);
+        System.out.println("Buscando conta: " + agency);
+        return ResponseEntity.ok(getCountByAgency(agency));
+    }
+
+    @GetMapping("/{cpf}")
+    public ResponseEntity<Count> get(@PathVariable Long cpf) {
+        //log.info("Buscando conta: " + agency);
+        System.out.println("Buscando conta: " + cpf);
+        return ResponseEntity.ok(getCountByCpf(cpf));
+    }
+
+    private Count getCountByAgency (String agency) {
+        return repository.findById(agency)
+                .orElseThrow(
+                        () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Conta não encontrada.")
+                );
+    }
+
+    private Count getCountByCpf (Long cpf) {
+        return repository.findByCpf(cpf).orElseThrow(
+                        () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Conta não encontrada.")
+                );
     }
 
 
--- src/main/java/br/com/fiap/project_digital_bank/repository/CountRepository.java
+++ src/main/java/br/com/fiap/project_digital_bank/repository/CountRepository.java
@@ -1,9 +1,13 @@
 package br.com.fiap.project_digital_bank.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.fiap.project_digital_bank.model.Count;
 
 public interface CountRepository extends JpaRepository<Count, String> {
+
+    Optional<Count> findByCpf(Long cpf);
     
 }
